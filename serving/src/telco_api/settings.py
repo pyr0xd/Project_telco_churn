40749c1050bae552c4fabdbd11522183f7851793
OscarK99Swe <oscar.kock99@gmail.com>
2026-09-15 15:42:57 +0200

    API done
    API is working! :D

--- serving/src/telco_api/settings.py
+++ serving/src/telco_api/settings.py
@@ -2,8 +2,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "Telco Churn API"
-    model_path: str = "artifacts/baseline/randomforest_best.joblib"
-    # Model Changing path example down below
-    # model_path: str = "artifacts/model.joblib"
+    #model_path: str = "artifacts/baseline/randomforest_best.joblib"
+    #Model Changing path example down below
+    model_path: str = "artifacts/model.joblib"
 
 settings = Settings()
